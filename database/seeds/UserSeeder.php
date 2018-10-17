83548d4f7da35470c164f3fd63bbf5a931735647
thavoo <gustavoh.2312@gmail.com>
2018-10-16 21:17:49 -0500

    Apply faker data for user

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $users = factory(App\User::class, 200)->create();
+      
+    }
+}
