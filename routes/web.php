1e267cd17440c6b0ce3fa91ebb1fec5afe96f42f
thavoo <gustavoh.2312@gmail.com>
2018-10-16 21:18:14 -0500

    process for get data json

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/lista', function() {
+    $data = App\User::all();
+    return view('listado', compact('data'));
+});
+
+Route::get('/datos', function() {
+        $data = App\User::all();
+
+        return response()->json(
+            [
+                'data' => $data
+                ]
+            );
+});
